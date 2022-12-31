621d0002a47fc2bf7b83dbf53746ac5094baca0c
Amal P Mathews <amalpmathews2003@gmail.com>
2022-12-31 16:00:41 +0530

    added workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://amalpmathews2003.github.io',
+  base: '/timetable-prettify',
   integrations: [react()]
 });
